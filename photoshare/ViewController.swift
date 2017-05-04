b936747844b2a6eccf9a4dbc5b48461b76956eaa
Shun-Ching <Angel@AngeldeMacBook-Pro.local>
2017-05-04 15:45:56 +0800

    Grabbing a Photo from an iPhone Gallery

--- photoshare/ViewController.swift
+++ photoshare/ViewController.swift
@@ -8,12 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    var picker:UIImagePickerController!
+    
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        Ostetso.showGallery()
+//        Ostetso.showGallery()
+        
+        picker = UIImagePickerController()
+        picker.delegate = self
+    }
+    
+    @IBAction func chooseButtonClicked(_ sender: Any) {
+        present(self.picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = pickedImage
+        }
+        dismiss(animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
