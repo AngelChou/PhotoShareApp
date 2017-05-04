88c768042296ec679db760e75651004f86158ff6
Shun-Ching <Angel@AngeldeMacBook-Pro.local>
2017-05-04 14:59:18 +0800

    install Objective-C framework into Swift Project

---
+++ photoshare/OstetsoBridge.h
@@ -0,0 +1,15 @@
+//
+//  OstetsoBridge.h
+//  photoshare
+//
+//  Created by Shun-Ching, Chou on 2017/5/4.
+//  Copyright © 2017年 Shun-Ching, Chou. All rights reserved.
+//
+
+#ifndef OstetsoBridge_h
+#define OstetsoBridge_h
+
+#import <UIKit/UIkit.h>
+#import "Ostetso/Ostetso.h"
+
+#endif /* OstetsoBridge_h */
